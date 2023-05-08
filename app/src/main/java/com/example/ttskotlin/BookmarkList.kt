cd6e4abdd25c2c976cfcf15e07874b2a8c8e396e
rifkiam <rifki.ahmad2003@gmail.com>
2023-05-08 18:41:51 +0700

    add ui placeholder for bookmark

---
+++ app/src/main/java/com/example/ttskotlin/BookmarkList.kt
@@ -0,0 +1,29 @@
+package com.example.ttskotlin
+
+import android.content.Intent
+import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.Request
+import android.widget.LinearLayout
+import org.json.JSONArray
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONObject
+import android.view.View
+import android.widget.TextView
+import android.graphics.Color
+import android.util.TypedValue
+import com.bumptech.glide.Glide
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import java.util.*
+
+class BookmarkList : AppCompatActivity() {
+
+}
